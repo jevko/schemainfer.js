2ba34c51739707449e46d3679dfa809bf3b66237
Dariusz Jędrzejczak <dariusz.jedrzejczak.work+github@gmail.com>
2022-02-04 00:20:02 +0100

    todo

---
+++ TODO.md
@@ -0,0 +1,16 @@
+Currently arrays are inferred to be tuples. Implement optional array inference by checking that all elements have the same schema. Sketch:
+
+```js
+// todo: use to implement array schema
+const areSchemasEqual = (a, b) => {
+  const {type} = a
+  if (type !== b.type) return false
+  
+  // note: assuming primitive types can't be refined further (won't be true in future)
+  if (['string', 'float64', 'boolean', 'null'].includes(type)) return true
+
+  if (type === 'tuple') return 'todo'
+  if (type === 'array') return 'todo'
+  if (type === 'object') return 'todo'
+}
+```
--- mod.js
+++ mod.js
@@ -1,44 +1,3 @@
-import {escape} from './utils.js'
-
-const transplit = (json, setSchema) => {
-  if (typeof json === 'string') {
-    setSchema('string')
-    return escape(json)
-  }
-  if (typeof json === 'number') {
-    setSchema('float64')
-    return json.toString()
-  }
-  if (typeof json === 'boolean') {
-    setSchema('boolean')
-    return json.toString()
-  }
-  if (json === null) {
-    setSchema('null')
-    return 'null'
-  }
-  if (Array.isArray(json)) {
-    // todo: if all items are the same type, make it an array
-    setSchema('tuple')
-    // todo: to jevko tree
-    let ret = []
-    for (const val of json) {
-      ret.push(transplit(val, setSchema))
-    }
-    return ret
-  }
-  // assume object
-  setSchema('object')
-  const entries = Object.entries(json)
-  // todo: to jevko tree
-  let ret = []
-  for (const val of json) {
-    ret.push(transplit(val), setSchema)
-  }
-  return ret
-}
-
-
 // infers schema from JSON
 export const jsonToSchema = (json) => {
   if (typeof json === 'string') {
@@ -68,19 +27,6 @@ export const jsonToSchema = (json) => {
     props[key] = jsonToSchema(val)
   }
   return {type: 'object', props}
-}
-
-// todo: use to implement array schema
-const areSchemasEqual = (a, b) => {
-  const {type} = a
-  if (type !== b.type) return false
-  
-  // note: assuming primitive types can't be refined further (won't be true in future)
-  if (['string', 'float64', 'boolean', 'null'].includes(type)) return true
-
-  if (type === 'tuple') return 'todo'
-  if (type === 'array') return 'todo'
-  if (type === 'object') return 'todo'
 }
 
 // infers schema from interjevko
