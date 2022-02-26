c33433b771f4c9cacecf5e4398888169c996fb2f
Dariusz Jędrzejczak <dariusz.jedrzejczak.work+github@gmail.com>
2022-02-26 04:13:15 +0100

    moved interJevkoToSchema to interjevko

--- deps.js
+++ deps.js
@@ -1,1 +1,2 @@
 export {jsonToSchema} from 'https://cdn.jsdelivr.net/gh/jevko/jsonjevko.js@v0.1.0/mod.js'
+export {interjevkoToSchema} from 'https://cdn.jsdelivr.net/gh/jevko/interjevko.js@v0.1.3/mod.js'
